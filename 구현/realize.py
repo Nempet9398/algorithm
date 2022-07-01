9897dbeefa94fd5bf2b6912639e7cbbea6939d1a
Nempet9398 <hahaha990516@gmail.com>
2022-07-01 15:47:00 +0900

    구현 수정

--- 구현/realize.py
+++ 구현/realize.py
@@ -61,23 +61,26 @@ print(result)
 ## 4 게임개발
 
 # /n,m = map(int, input().split())
-m = 4
-n = 4
-all_map = [[0] * m for i in range(n)]
+m = 5
+n = 5
+d = [[0] * m for i in range(n)]
 
-x,y, direction = map(int,input().split())
+# x,y, direction = map(int,input().split())
+x =2
+y =2
+direction = 0
+# all_map[x][y] = 1
 
-all_map[x][y] = 1
-
-loc_array = []
+array = []
 # for i in range(n):
 #     array.append(list(map(int, input().split)))
-loc_array.append([1,1,1,1])
-loc_array.append([1,0,0,1])
-loc_array.append([1,1,0,1])
-loc_array.append([1,1,1,1])
+array.append([1,1,1,1,1])
+array.append([1,0,0,1,1])
+array.append([1,0,0,0,1])
+array.append([1,1,0,0,1])
+array.append([1,1,1,1,1])
 
-print('야')
+
 ## 북 동 남 서 정의
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0 , -1]
@@ -89,24 +92,41 @@ def turn_left():
     if direction == -1:
         direction = 3
 
+count = 0
+turn_time = 0
+
+while True:
+
+    # 주어진 방향에서 왼쪽으로 돌기
+    turn_left()
+    turn_time += 1
+    # 돌고 앞으로 가기
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+
+    if d[nx][ny] ==0 and array[nx][ny] ==0:
+        d[nx][ny]=1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+
+        continue
+    else:
+        turn_time += 1
+
+    if turn_time ==4:
+        nx = x -dx[direction]
+        ny = y -dy[direction]
+        if array[nx][ny] ==0:
+            x = nx
+            y = ny
+
+        else:
+            break
+        turn_time= 0
+
+    # 앞으로 갔는데 1이면 다시 돌아오기
 
 
-#
-# count = 1
-# turn_time = 0
-#
-# while True:
-#     turn_left():
-#     nx = x + dx[direction]
-#     ny = y + dy[direction]
-#
-#     if d[nx][ny] == 0 and array[nx][ny] ==0:
-#         d[nx][ny]=1
-#         x = nx
-#         y = ny
-#         count += 1
-#         turn_time = 0
-#         continue
-#
-#     else:
-#         turn_time += 1
+print(count, 'result')

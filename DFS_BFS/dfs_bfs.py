572fd4c9f80d1fe7aa3e3ea729c0d8c2f010316e
Nempet9398 <hahaha990516@gmail.com>
2022-07-13 12:31:16 +0900

    DFS 알고리즘 추가

--- DFS_BFS/dfs_bfs.py
+++ DFS_BFS/dfs_bfs.py
@@ -37,3 +37,37 @@ graph = [
 ]
 
 #3-1 인접 리스트
+# 행이 3개인 2차원 인접리스트
+graph = [[] for _ in range(3)]
+graph[0].append((1,7))
+graph[0].append((2,5))
+graph[1].append((0,7))
+graph[2].append((0,5))
+
+print(graph)
+
+#4 DFS 예제
+
+graph =[
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+def DFS(graph,v,visited):
+    visited[v] = factorial_number
+    print(v, end=' ')
+    #다른 노드 재귀방문
+    for i in graph[v]:
+        if not visited[i]:
+            DFS(graph,i,visited)
+
+visited = [False] * 9
+
+DFS(graph,1,visited)

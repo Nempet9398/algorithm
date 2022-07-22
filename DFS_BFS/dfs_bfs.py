b968887ac422ef19261b12cc59e3406c739a2849
Nempet9398 <hahaha990516@gmail.com>
2022-07-22 17:53:24 +0900

    미로찾기 예제 추가

--- DFS_BFS/dfs_bfs.py
+++ DFS_BFS/dfs_bfs.py
@@ -136,3 +136,46 @@ for i in range(n):
             result += 1
 
 print(result , ' : 아이스크림 개수')
+
+
+##7 미로 찾기
+
+n = 6
+m = 5
+
+graph = [
+    [1,0,1,0,0],
+    [1,1,1,0,0],
+    [0,1,1,0,1],
+    [1,1,0,0,1],
+    [0,1,1,1,1],
+    [0,1,1,1,1]
+]
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+
+    # 큐가 빌 떄 까지
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or ny < 0 or nx >=n or ny >= m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny]==1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+
+    return graph[n-1][m-1]
+
+print((bfs(0,0)), '답이다')
